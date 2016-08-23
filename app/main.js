6d61f018ac3ee1fe879c3b0de45fb95e1d3918ab
guodong <gd@tongjo.com>
2016-08-23 18:26:51 +0800

    update

--- app/main.js
+++ app/main.js
@@ -2,11 +2,12 @@ import {Desktop} from './components/Desktop';
 import {Menubar} from './components/Menubar';
 import './styles/main.less';
 
+var menubar = new Menubar();
 
 var desktop = new Desktop({
-  background: 'url(assets/windows7-bg.jpg)'
+  background: 'url(assets/windows7-bg.jpg)',
+  content: [menubar]
 });
 desktop.show();
 
-var menubar = new Menubar();
-menubar.show();
+//menubar.show();
